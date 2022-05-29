2dc1aa2355eebdc57f53f612bbacd9b87ae70579
Avik <avikmakwana81@gmail.com>
2022-05-29 22:25:19 +0530

    Now Note can save in Database {not show on main screen we will set it }

--- app/src/main/java/com/example/aviknotes/activities/CreateNoteActivity.java
+++ app/src/main/java/com/example/aviknotes/activities/CreateNoteActivity.java
@@ -1,14 +1,30 @@
 package com.example.aviknotes.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.AsyncTaskLoader;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.aviknotes.R;
+import com.example.aviknotes.database.NoteDatabase;
+import com.example.aviknotes.entities.Note;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class CreateNoteActivity extends AppCompatActivity {
+
+    private EditText inputNoteTitle, inputNoteSubtitle, inputNoteText;
+    private TextView textDateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +38,69 @@ public class CreateNoteActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+
+//        Declaration
+
+        inputNoteTitle = findViewById(R.id.inputNoteTitle);
+        inputNoteSubtitle = findViewById(R.id.inputNoteSubtitle);
+        inputNoteText = findViewById(R.id.inputNote);
+
+        textDateTime = findViewById(R.id.textDateTime);
+
+
+//        Setting up Date & TIme format : (Saturday, 29 May 2022 21:59 PM)
+        textDateTime.setText(
+                new SimpleDateFormat("EEEE, dd MMMM yyyy  HH:mm a", Locale.getDefault())
+                .format(new Date())
+        );
+
+        //Set Validation for check mark
+
+        ImageView imageSave = findViewById(R.id.imageSave);
+        imageSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveNote();
+            }
+        });
+    }
+
+    private void  saveNote(){
+        //Set some validation
+        if(inputNoteTitle.getText().toString().trim().isEmpty()){
+            Toast.makeText(this, "Note title can't be empty !", Toast.LENGTH_SHORT).show();
+            return;
+        }else if(inputNoteSubtitle.getText().toString().trim().isEmpty() && inputNoteText.getText().toString().trim().isEmpty()){
+            Toast.makeText(this, "Note can't be empty !   ", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        final Note note = new Note();
+        note.setTitle(inputNoteTitle.getText().toString());
+        note.setSubtitile(inputNoteSubtitle.getText().toString());
+        note.setNotetext(inputNoteText.getText().toString());
+        note.setDateTime(textDateTime.getText().toString() );
+
+        @SuppressLint("StaticFieldLeak")
+        class SaveNoteTask extends AsyncTask<Void, Void, Void> {
+
+            @Override
+            protected Void doInBackground(Void... voids){
+                NoteDatabase.getDatabase(getApplicationContext()).noteDao().insertNote(note);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid){
+                super.onPostExecute(aVoid);
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+
+        }
+
+        new SaveNoteTask().execute();
     }
 }
