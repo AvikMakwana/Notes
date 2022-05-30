1c1b8450a345a58d657d2800d5bd48f571796e7b
Avik <avikmakwana81@gmail.com>
2022-05-30 18:46:37 +0530

    Delete Note Function Implemented

--- app/src/main/java/com/example/aviknotes/activities/CreateNoteActivity.java
+++ app/src/main/java/com/example/aviknotes/activities/CreateNoteActivity.java
@@ -60,6 +60,7 @@ public class CreateNoteActivity extends AppCompatActivity {
     private  static final int REQUEST_CODE_SELECT_IMAGE = 2;
 
     private AlertDialog dialogAddURL;
+    private AlertDialog dialogDeleteNote;
 
     private  Note alreadyAvailableNote;
 
@@ -342,6 +343,70 @@ public class CreateNoteActivity extends AppCompatActivity {
 
               }
           });
+
+          if (alreadyAvailableNote != null){
+              layoutMiscellaneous.findViewById(R.id.layoutDeleteNote).setVisibility(View.VISIBLE);
+              layoutMiscellaneous.findViewById(R.id.layoutDeleteNote).setOnClickListener(new View.OnClickListener() {
+                  @Override
+                  public void onClick(View view) {
+                      bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                      showDeleteNoteDialog();
+
+                  }
+              });
+          }
+
+    }
+
+    //Function to show Dialog box
+    private void showDeleteNoteDialog(){
+
+        if (dialogDeleteNote == null){
+            AlertDialog.Builder builder = new AlertDialog.Builder(CreateNoteActivity.this);
+            View view = LayoutInflater.from(this).inflate(
+                    R.layout.layout_delete_note,
+                    (ViewGroup) findViewById(R.id.layoutDeleteNoteContainer)
+            );
+            builder.setView(view);
+            dialogDeleteNote = builder.create();
+            if (dialogDeleteNote.getWindow() != null){
+                dialogDeleteNote.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+            }
+            view.findViewById(R.id.textDeleteNote).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    @SuppressLint("StaticFieldLeak")
+                    class DeleteNoteTask extends AsyncTask<Void, Void, Void>{
+
+                        @Override
+                        protected Void doInBackground(Void... voids) {
+                            NoteDatabase.getDatabase(getApplicationContext()).noteDao()
+                                    .deleteNote(alreadyAvailableNote);
+                            return null;
+                        }
+
+                        @Override
+                        protected void onPostExecute(Void aVoid) {
+                            super.onPostExecute(aVoid);
+                            Intent intent = new Intent();
+                            intent.putExtra("isNoteDeleted", true);
+                            setResult(RESULT_OK, intent);
+                            finish();
+                        }
+                    }
+
+                    new DeleteNoteTask().execute();
+                }
+            });
+            view.findViewById(R.id.textCancel).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialogDeleteNote.dismiss();
+                }
+            });
+        }
+
+        dialogDeleteNote.show();
     }
 
 //    Function to set Subtitle Indicator Color
